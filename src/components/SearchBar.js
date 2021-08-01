2756216ded6b46c623a0b40b3430ea5a79de0c28
wangchhu <wangchhutamang123@gmail.com>
2021-08-01 17:49:17 +0545

    Search js

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function SearchBar() {
+    return (
+        <div>
+                <div class="c1">
+                <div class="para1">
+                    <div class="a1">
+                    <a>Find Deals on Hotel now</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="f1">
+            <input id="textf1" type="text" placeholder="Search your destination" width="30" height="30"/>
+            <button id="textf1">Search</button>        
+            </div>
+            <div class="a2">
+                <a>From cosy country homes to funky city flats</a>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar
