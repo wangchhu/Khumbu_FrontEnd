e87cae3a59623879b1b457c7fa9786afa369f032
wangchhu <wangchhutamang123@gmail.com>
2021-08-01 20:38:03 +0545

    Search feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 
 import{BrowserRouter} from 'react-router-dom';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <BrowserRouter>
      <Navbar></Navbar>
+     <SearchBar></SearchBar>
     </BrowserRouter>
   );
 } 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './SearchBar.css';
 
 function SearchBar() {
     return (
         <div>
                 <div class="c1">
+                
                 <div class="para1">
                     <div class="a1">
-                    <a>Find Deals on Hotel now</a>
+                    <p>Find Deals on Hotel now</p>
                     </div>
                 </div>
             </div>
 
             <div class="f1">
-            <input id="textf1" type="text" placeholder="Search your destination" width="30" height="30"/>
-            <button id="textf1">Search</button>        
+            <input id="textf1" type="text" placeholder="Search your destination" width="30" height="30"/>    
+            </div>
+            <div class="f2">
+            <button class="btn-margin">Search</button>    
             </div>
             <div class="a2">
-                <a>From cosy country homes to funky city flats</a>
+                <p>From cosy country homes to funky city flats</p>
             </div>
+            
         </div>
     )
 }
 
-export default SearchBar
+export default SearchBar;
