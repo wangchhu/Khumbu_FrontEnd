f0f2a8519e060fc7082f7b46f802a9c62e58196a
wangchhu <wangchhutamang123@gmail.com>
2021-08-02 16:42:07 +0545

    Destination

---
+++ src/components/Destination.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Destination() {
+    return (
+        <div>
+            <div class="container">
+                    <div class="d2">
+                        <h3>These top destinations are just a click away</h3>
+                    </div>
+                <div id="d2-1">
+                    <a>Top Cities</a>
+                    <div class="cities">
+                        <div id="cities-1">
+                            <img src="Public/los.jpg" alt="" height="250px" width="250px"/>
+                            <h6>Los Angeles</h6>
+                            <p>5,990 Hotels <p id="av">Avg. $120</p></p>
+                        </div>
+                        {/* <div id="cities-1">
+                            <img src="Public/los.jpg" alt="" height="250px" width="250px">
+                            <h6>Los Angeles</h6>
+                            <p>5,990 Hotels <a id="av">Avg. $175</a></p>
+                        </div>
+                        <div id="cities-1">
+                            <img src="Public/los.jpg" alt="" height="250px" width="250px">
+                            <h6>Los Angeles</h6>
+                            <p>5,990 Hotels <a id="av">Avg. $200</a></p>
+                        </div>
+                        <div id="cities-1">
+                            <img src="Public/los.jpg" alt="" height="250px" width="250px">
+                            <h6>Los Angeles</h6>
+                            <p>5,990 Hotels <a id="av">Avg. $300</a></p>
+                        </div> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Destination
