f5a4edf61c4be60c4bcfa1021b4d2fd421d22dfc
wangchhu <wangchhutamang123@gmail.com>
2021-09-02 20:37:37 +0545

    Booking form

--- src/Pages/Book.js
+++ src/Pages/Book.js
@@ -31,6 +31,8 @@ class Book extends Component{
             console.log(err.response)
         })
     }
+
+
     render(){
         return(
             <div>           
@@ -40,7 +42,7 @@ class Book extends Component{
                 <fieldset>  
                     <legend>Personal Details</legend>
               <label for="name" id="name">Username</label>  
-              <input type="text" id="name" name="client_Username" placeholder="username" pattern="[a-zA-Z]" value={this.state.client_Username} onChange={(event)=>{this.setState({client_Username:event.target.value})}}/>
+              <input type="text" id="name" name="client_Username" placeholder="username"  value={this.state.client_Username} onChange={(event)=>{this.setState({client_Username:event.target.value})}}/>
               <label for="email">E-mail</label>  
               <input type="text" id="email" name="Email" placeholder="e-mail" value={this.state.Email} onChange={(event)=>{this.setState({Email:event.target.value})}}/>
               <label for="phone">Phone</label>  
@@ -51,38 +53,38 @@ class Book extends Component{
              <div class="Booking">
              <fieldset>
                  <legend>Booking Details</legend>
-              <select name="Rooms">
+              <select name="Rooms" value={this.state.Rooms} onChange={(event)=>{this.setState({Rooms:event.target.value})}}>
                   <option value="">Rooms</option>
-                  <option value="">1</option>
-                  <option value="">2</option>
-                  <option value="">3</option>
-                  <option value="">4</option>
-                  <option value="">5</option>
-                  <option value="">6</option>
-                  <option value="">7</option>
-                  <option value="">8</option>
-                  <option value="">9</option>
-                  <option value="">10</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
               </select>
               <label for="">Check-in</label>  
-              <input type="date" placeholder="Check-in" min="2021-05-05"/>
+              <input type="date" name="Check_in" placeholder="Check-in" min="2021-05-05" value={this.state.Check_in} onChange={(event)=>{this.setState({Check_in:event.target.value})}}/>
               <label for="">Check-out</label>  
-              <input type="date" placeholder="Check-out"/>
-             <select name="Guest" required>
+              <input type="date" name="Check_out "placeholder="Check-out" value={this.state.Check_out} onChange={(event)=>{this.setState({Check_out:event.target.value})}}/>
+             <select name="Guest" value={this.state.Guest} onChange={(event)=>{this.setState({Guest:event.target.value})}} required>
                   <option value="">Guests</option>
-                  <option value="">1</option>
-                  <option value="">2</option>
-                  <option value="">3</option>
-                  <option value="">4</option>
-                  <option value="">5</option>
-                  <option value="">6</option>
-                  <option value="">7</option>
-                  <option value="">8</option>
-                  <option value="">9</option>
-                  <option value="">10</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
               </select>
               <br/>
-              <button id="BtnSubmit">Submit</button>
+              <button id="BtnSubmit" onClick={this.SendUserData}>Submit</button>
               </fieldset>
               </div>
             </form>
