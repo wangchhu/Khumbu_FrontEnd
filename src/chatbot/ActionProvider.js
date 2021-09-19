2a1ce28a691717f0cd8e7634a65ba0399c23b764
wangchhu <wangchhutamang123@gmail.com>
2021-09-20 00:14:48 +0545

    updated ux

--- src/chatbot/ActionProvider.js
+++ src/chatbot/ActionProvider.js
@@ -9,6 +9,16 @@ class ActionProvider {
       this.addMessageToState(message);
     };
   
+    greet1 = () => {
+        const message = this.createChatBotMessage("Tell us your budget range");
+        this.addMessageToState(message);
+      };
+
+    greet2 = () => {
+        const message = this.createChatBotMessage("1.Hotel Tibet 2. Hotel Everest 3. Hyatt");
+        this.addMessageToState(message);
+      };
+
     addMessageToState = (message) => {
       this.setState((prevState) => ({
         ...prevState,
--- src/chatbot/MessageParser.js
+++ src/chatbot/MessageParser.js
@@ -6,9 +6,18 @@ class MessageParser {
     parse(message) {
       console.log(message);
       const lowercase = message.toLowerCase();
+    
   
       if (lowercase.includes("hello")) {
         this.actionProvider.greet();
+      }
+
+      if (lowercase.includes("suggest a hotel")) {
+        this.actionProvider.greet1();
+      }
+
+      if (lowercase.includes("hotel near boudha")) {
+        this.actionProvider.greet2();
       }
   
     }
